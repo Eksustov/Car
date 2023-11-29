bf00ac064f7a3b86f9e2a143710bc799ae731014
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-11-29 08:56:14 +0200

    Riepa pievienota

--- index.php
+++ index.php
@@ -11,9 +11,13 @@ $bird2 = new Bird("Matis", 52);
 echo "$bird2->name <br>";*/
 
 include "Car.php";
+include "Tire.php";
 
 $car1 = new Car("Skoda", 256);
-
 $car2 = new Car("Bugartu", 267);
+
 $car2->increaseMileage(240);
+
+$tire1 = new Tire(25, "Winter", "Battle-scarred");
+echo "<br>Size: $tire1->size <br> Type: $tire1->type <br>";
 ?>
